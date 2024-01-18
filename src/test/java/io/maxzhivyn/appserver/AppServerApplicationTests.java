9603546bb423f526b529ef3a90a2c9f8a5325c77
ZhivMaxHub <maxzhiv19@gmail.com>
2024-01-18 12:08:31 +0400

    init

---
+++ src/test/java/io/maxzhivyn/appserver/AppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.maxzhivyn.appserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
